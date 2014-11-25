f96b859c659a7bf31554da1488c9547f646bc921
JovanZDR <Jovanzdrave@gmail.com>
2014-11-25 12:00:54 +0100

    not significant change

---
+++ js/registerSoldBooks.js
@@ -0,0 +1,42 @@
+$(function() {
+
+  function registerSoldBooks(thisForm) {
+
+    var soldBookRegisteringInfo = {};
+
+    $(thisForm).find("input").not("input[type='submit']").each(function() {
+      soldBookRegisteringInfo[this.name] = $(this).val();
+
+    });
+      console.log(soldBookRegisteringInfo);
+
+   $.ajax({
+   
+      url:"../libs/sql-ajax-json.php",
+   
+      dataType: "json",
+      data: {
+        sql: "sql/sql-questions.sql",
+        run: "register soldbooks",
+        
+        isbn: soldBookRegisteringInfo["isbn"],
+        qty: soldBookRegisteringInfo["qty"]
+ 
+      },
+      success: function(data) {
+        console.log("registerSoldBooks success: ", data);
+        $(".registeredSoldBooks").show();
+      },
+      error: function(data) {
+        console.log("great error",data.responseText);
+        $(".cancelSoldRegistering").show();
+      }
+    });
+  }
+
+  $(".soldBookRegisteringInfo").submit(function() {
+    registerSoldBooks(this);
+
+    return false;
+  });
+});
