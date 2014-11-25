b6625e739fa3f7ef82a9ef59c34f93607b82e09b
Jallnnn <Jallnnn@Jallnnns-MacBook-Air.local>
2014-11-25 11:42:58 +0100

    Made some changes to js. Changed amount to qty (quantity).

--- sql/sql-questions.sql
+++ sql/sql-questions.sql
@@ -8,4 +8,4 @@
 INSERT INTO book (ISBN, publisherPrice, quantity, shelf) VALUES ({ISBN}, {publisherPrice}, {quantity}, {shelf});
 
 # register soldbooks
-INSERT INTO sale (isbn, amount) VALUES ({isbn}, {amount});
+INSERT INTO sale (isbn, qty) VALUES ({isbn}, {qty});
