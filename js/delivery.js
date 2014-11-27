19482b3119eea66596c59bf9b0cd7235d9370af5
JovanZDR <Jovanzdrave@gmail.com>
2014-11-27 13:13:01 +0100

    changed the positions of the  expressions inside the if sats

--- js/delivery.js
+++ js/delivery.js
@@ -12,12 +12,12 @@ $function() {
 
       },
       success: function (data) {
-        if (formInfo["fname"] != data[0]["fname"] || formInfo["lname"] != data[0]["lname"]) {
-          registerAuthors(formInfo);
-//if the author exists, we can export the fname and lname from here("else"), 
-//we just need to create a loop and export to the html(possibly in "<input>").
-        else {
+//if the author exists, we can export the fname and lname from here("inside "if{}"), 
+//we just need to create a loop and export it to the html(possibly in "<input>").        
+        if (formInfo["fname"] == data[0]["fname"] || formInfo["lname"] == data[0]["lname"]) {
 
+        else {
+          registerAuthors(formInfo);
         }
       },
       error: function(data) {
